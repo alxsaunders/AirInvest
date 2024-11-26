952666c23bf3ba03571ba5240fc8ca99c8f141f8
Alx <alex@Alxs-MacBook-Pro.local>
2024-11-26 11:50:34 -0500

    column for single results page

--- src/components/property/PropertyDetail.tsx
+++ src/components/property/PropertyDetail.tsx
@@ -26,7 +26,7 @@ export function PropertyDetail({ property }: PropertyDetailProps) {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-[90rem] mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="flex justify-between items-center mb-8">
           <Link
             href="/results"
@@ -36,46 +36,65 @@ export function PropertyDetail({ property }: PropertyDetailProps) {
           </Link>
         </div>
 
-        <div className="space-y-8">
-          {property.originalPhotos && property.originalPhotos.length > 0 && (
-            <div className="rounded-lg overflow-hidden">
-              <PhotoGallery photos={property.originalPhotos} />
-            </div>
-          )}
+        {/* Main content grid */}
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+          {/* Left column - Property Details */}
+          <div className="lg:col-span-7 space-y-7">
+            {property.originalPhotos && property.originalPhotos.length > 0 && (
+              <div className="rounded-lg overflow-hidden">
+                <PhotoGallery photos={property.originalPhotos} />
+              </div>
+            )}
 
-          <Card className="bg-gray-800/50 border-gray-700">
-            <CardHeader>
-              <CardTitle className="text-2xl text-white">
-                {property.address.streetAddress}
-              </CardTitle>
-              <p className="text-gray-300">
-                {property.address.city}, {property.address.state} {property.address.zipcode}
-              </p>
-            </CardHeader>
-            <CardContent>
-              <PropertyStats property={property} />
-            </CardContent>
-          </Card>
-
-          {property.priceHistory && property.priceHistory.length > 0 && (
             <Card className="bg-gray-800/50 border-gray-700">
               <CardHeader>
-                <CardTitle className="text-white">Price History</CardTitle>
+                <CardTitle className="text-2xl text-white">
+                  {property.address.streetAddress}
+                </CardTitle>
+                <p className="text-gray-300">
+                  {property.address.city}, {property.address.state} {property.address.zipcode}
+                </p>
               </CardHeader>
               <CardContent>
-                <PriceHistory priceHistory={property.priceHistory} />
+                <PropertyStats property={property} />
               </CardContent>
             </Card>
-          )}
 
-          <Card className="bg-gray-800/50 border-gray-700">
-            <CardHeader>
-              <CardTitle className="text-white">Location</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <PropertyLocation property={property} />
-            </CardContent>
-          </Card>
+            {property.priceHistory && property.priceHistory.length > 0 && (
+              <Card className="bg-gray-800/50 border-gray-700">
+                <CardHeader>
+                  <CardTitle className="text-white">Price History</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <PriceHistory priceHistory={property.priceHistory} />
+                </CardContent>
+              </Card>
+            )}
+
+            <Card className="bg-gray-800/50 border-gray-700">
+              <CardHeader>
+                <CardTitle className="text-white">Location</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <PropertyLocation property={property} />
+              </CardContent>
+            </Card>
+          </div>
+
+          {/* Right column - Investment Details */}
+          <div className="lg:col-span-5 space-y-5">
+            <Card className="bg-gray-800/50 border-gray-700 sticky top-8">
+              <CardHeader>
+                <CardTitle className="text-white">Investment Details</CardTitle>
+              </CardHeader>
+              <CardContent>
+                {/* Add your InvestmentDetails component here */}
+                <div className="text-gray-300">
+                  Investment calculator and details will go here
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
